c76e0a2c1dcaaad86b9aae655cd37cf976a76398
pablolamenza <pablolamenza251@gmail.com>
2022-10-02 19:31:04 -0300

    asf

--- Programacion/Gestor.ts
+++ Programacion/Gestor.ts
@@ -8,30 +8,32 @@ class Registro{
     Motor:any; 
     DeudaPatente:boolean;
     Multas:boolean;
+
 mostrarInfo(){
   console.log(`Data info de automotor:this.Nombre ${this.Nombre} , this.Apellido${this.Apellido} , this.Auto${this.Auto} , this.Modelo${this.Modelo} , this.Patente${this.Patente} , this.Chasis${this.Chasis} , this.Motor${this.Motor} , this.DeudaPatente${this.DeudaPatente} , This.Multas${this.Multas}`) ;
 
 }
 
 constructor(
-   Nombre:string,
-   Apellido:string,
-   Auto:string,
-   Modelo:any,
-   Patente:any,
-   Chasis:any,
-   Motor:any,
-   DeudaPatente:boolean,
-   Multas:boolean,)
-   {this.Nombre = Nombre
-    this.Apellido = Apellido
-    this.Auto = Auto
-    this.Modelo = Modelo
-    this.Patente = Patente
-    this.Chasis = Chasis
-    this.Motor = Motor
-    this.DeudaPatente = DeudaPatente
-    this.Multas = Multas}
+  Nombre:string,
+  Apellido:string,
+  Auto:string,
+  Modelo:any,
+  Patente:any,
+  Chasis:any,
+  Motor:any,
+  DeudaPatente:boolean,
+  Multas:boolean,)
+  {this.Nombre = Nombre
+   this.Apellido = Apellido
+   this.Auto = Auto
+   this.Modelo = Modelo
+   this.Patente = Patente
+   this.Chasis = Chasis
+   this.Motor = Motor
+   this.DeudaPatente = DeudaPatente
+   this.Multas = Multas}
+
 
 }
 
